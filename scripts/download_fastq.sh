a81d827f2f08ded3a25c95436da8d72bbb71b09a
mtd1997 <100222062+mtd1997@users.noreply.github.com>
2025-07-28 16:14:52 +0000

    Delete scripts/download_fastq.sh

--- scripts/download_fastq.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install gdown if not already available
-pip3 install --quiet gdown
-
-# Download the shared Google Drive folder containing fastq files
-echo "Downloading FASTQ files from Google Drive..."
-gdown --folder https://drive.google.com/drive/folders/1cWvepn5pRhaCSmLTQhh_Kr1A0PJtsCWK?usp=sharing -O data/fastq_files
